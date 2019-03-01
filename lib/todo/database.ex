0f9656069a2b97d84313d1cb29a43a6751bea2db
Tyler Liu <tyler@nanoleaf.me>
2019-02-28 23:05:12 -0500

    added distribute data store for the database store

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -15,10 +15,23 @@ defmodule Todo.Database do
   end
 
   def child_spec(_) do
+    db_settings = Application.fetch_env!(:todo, :database)
+
+    # Node name is used to determine the database folder. This allows us to
+    # start multiple nodes from the same folders, and data will not clash.
+    [name_prefix, _] = "#{node()}" |> String.split("@")
+    db_folder = "#{Keyword.fetch!(db_settings, :folder)}/#{name_prefix}/"
+
+    File.mkdir_p!(db_folder)
+
     :poolboy.child_spec(
       __MODULE__,
-      [name: {:local, __MODULE__}, worker_module: Todo.DatabaseWorker, size: 3],
-      [@db_folder]
+      [
+        name: {:local, __MODULE__},
+        worker_module: Todo.DatabaseWorker,
+        size: Keyword.fetch!(db_settings, :pool_size)
+      ],
+      [db_folder]
     )
   end
 
@@ -32,6 +45,19 @@ defmodule Todo.Database do
   end
 
   def store(key, data) do
+    {_results, bad_nodes} =
+      :rpc.multicall(
+        __MODULE__,
+        :store_local,
+        [key, data],
+        :timer.seconds(5)
+      )
+
+    Enum.each(bad_nodes, &IO.puts("Store failed on node #{&1}"))
+    :ok
+  end
+
+  def store_local(key, data) do
     :poolboy.transaction(
       __MODULE__,
       fn worker_pid ->
