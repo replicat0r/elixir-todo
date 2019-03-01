a5bfeb722242171507cca2aea6f5df44cf2e8e25
Tyler Liu <tyler@nanoleaf.me>
2019-02-28 22:38:59 -0500

    added web interface to todo

--- lib/todo/system.ex
+++ lib/todo/system.ex
@@ -8,7 +8,7 @@ defmodule Todo.System do
   @impl Supervisor
   def init(_) do
     Supervisor.init(
-      [Todo.Metrics, Todo.ProcessRegistry, Todo.Database, Todo.Cache],
+      [Todo.Metrics, Todo.ProcessRegistry, Todo.Database, Todo.Cache, Todo.Web],
       strategy: :one_for_one
     )
   end
